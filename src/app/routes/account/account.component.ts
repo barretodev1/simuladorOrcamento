2b15bc2aef4301716324502c5d0712e5015e49ac
barretodev1 <caiobarreto1acim@gmail.com>
2026-02-04 15:49:23 -0300

    add: adicionar a tela de criar conta

--- src/app/routes/account/account.component.ts
+++ src/app/routes/account/account.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { CardAccountt } from '../../components/card-accountt/card-account';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-account',
-  imports: [Header, CardAccountt],
+  imports: [Header, CardAccountt, CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './account.component.html'
 })
-export class AccountComponent {
 
+
+export class AccountComponent {
+  submitted = false;
+  email = new FormControl('', [Validators.required, Validators.email]);
+  onSubmit() {
+    this.submitted = true;
+
+    // força a validação aparecer ao clicar no botão
+    this.email.markAsTouched();
+
+    if (this.email.invalid) return;
+
+  }
 }
