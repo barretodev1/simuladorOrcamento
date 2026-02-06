9262418f796955bc0a1e70abb166cabb27fd64fd
barretodev1 <caiobarreto1acim@gmail.com>
2026-02-06 00:24:59 -0300

    add: add a funcionalidade login

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { AccountComponent } from './routes/account/account.component';
 import { HomeComponent } from './routes/home/home.component';
+import { SimuladorPageComponent } from './routes/simulador-page/simulador-page.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'account', component: AccountComponent }
-    // { path: 'simulador_de_gastos', component: SimuladorComponent }
+    { path: 'account', component: AccountComponent },
+    { path: 'simulador_de_gastos', component: SimuladorPageComponent }
 ];
